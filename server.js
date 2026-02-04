91fe26dc906bb7060618e8bad301ea640352b714
Dark <sudeep1098@gmail.com>
2026-02-05 00:13:42 +0530

    Initialize project

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import healthRoute from "./routes/health.routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.use('/', (req, res) => {
+  res.send('Welcome to the Image Processor API');
+});
+
+app.use("/health", healthRoute);
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
