ad2b26e28ed163fa9233951db5e7d44f76a928cd
Dark <sudeep1098@gmail.com>
2026-02-05 00:27:17 +0530

    add root route

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
+app.use("/health", healthRoute);
+
 app.use('/', (req, res) => {
   res.send('Welcome to the Image Processor API');
 });
 
-app.use("/health", healthRoute);
-
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-})
+// app.post('/profile', upload.single('avatar'), function (req, res, next) {
+//   // req.file is the `avatar` file
+//   // req.body will hold the text fields, if there were any
+// })
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
